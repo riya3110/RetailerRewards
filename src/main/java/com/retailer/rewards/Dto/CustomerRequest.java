beefaa595ae044599542e52c73d0de850e1547d3
RIYA <RIYA@gmail.com>
2025-06-20 01:36:26 +0530

    commit

--- src/main/java/com/retailer/rewards/Dto/CustomerRequest.java
+++ src/main/java/com/retailer/rewards/Dto/CustomerRequest.java
@@ -1,18 +1,14 @@
 package com.retailer.rewards.Dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class CustomerRequest {
 	
 	@NotBlank(message = "Name must not be blank")
 	private String name;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 
 }
